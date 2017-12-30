1bd675830f253865c5e81c0643724cc3de579e87
John <jmseymour111@gmail.com>
2017-12-29 20:47:35 -0800

    removed typo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { SettingsPage } from '../pages/settings/settings';
 
 import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';\
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
     declarations: [
