1766297890b03fb5143ea305c6f751ed3f3e2cba
John <jmseymour111@gmail.com>
2018-03-04 14:42:49 -0800

    signin and signup working now as observables

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,8 +3,12 @@ import { HttpClient } from '@angular/common/http';
 import firebase from 'firebase';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/filter';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/of';
+import { Observable } from 'rxjs/Observable';
 import { UserService } from './user.service';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthService {
@@ -29,62 +33,34 @@ export class AuthService {
     *****************************/
     public signup(name: string, email: string, password: string) {
         const newUser = { name, email, password };
-        let userData: any = {};
 
-        // Post new user to Database
-        return new Promise((resolve, reject) => {
-            this.http.post('https://housepal-server.herokuapp.com/users/signup', newUser).subscribe(
-                (result: any) => {
-                    console.log('Successfully created new user: ', result);
-                    userData = result;
-                    resolve();
-                },
-                error => reject(error)
-            );
-        })
-            .then(() => {
-                this.signin(email, password, userData.user);
-            })
-            .catch(error => {
-                console.error('Error creating new user: ', error);
-            });
+        return this.http.post('https://housepal-server.herokuapp.com/users/signup', newUser)
+            .catch(err => {
+                console.error('Error creating new user: ', err);
+                return Observable.throw(err);
+            }).switchMap(newUserData => this.firebaseSignin(email, password, newUserData.user));
     }
 
-    public signin(email: string, password: string, newUserData?: any) {
-        return new Promise((resolve, reject) => {
-            // If there is newUserData coming from Signup, resolve to next step.
-            newUserData
-                ? resolve(newUserData)
-                : // Otherwise, start Signin through Database to get userData.
-                this.http.post('https://housepal-server.herokuapp.com/users/signin', { email, password }).subscribe(
-                    (result: any) => {
-                        // Result = Array containing User object
-                        result.length ? resolve(result[0]) : reject('Signin to Database Failed');
-                    },
-                    error => reject(error)
-                );
-        })
-            .then(userData => {
-                // Sign in to Firebase Auth
-                return firebase
-                    .auth()
-                    .signInWithEmailAndPassword(email, password)
-                    .then(result => {
-                        // Authentication succcessful: Set this.currentUser, this.userToken, and this.authenticated.
-                        const firebaseUser = result.toJSON();
-                        this.currentUser = userData; // TODO: Can remove this? User Service works here!
-                        this.userService.setActiveUser(userData);
-                        this.userToken = firebaseUser.stsTokenManager.accessToken;
-                        this.authenticated = true;
-                    })
-                    .catch(error => {
-                        console.error('Error signing in to Firebase Auth', error);
-                        return error;
-                    });
-            })
-            .catch(error => {
-                console.error('Error with Signin. Please Try Again:', error);
-                return error;
+    public signin(email: string, password: string) {
+        return this.http.post('https://housepal-server.herokuapp.com/users/signin', { email, password })
+            .catch(err => {
+                console.error('Signin to Database Failed: ', err);
+                return Observable.throw(err);
+            }).switchMap(userData => this.firebaseSignin(email, password, userData));
+    }
+
+    public firebaseSignin(email, password, userData) {
+        return Observable.from(firebase.auth().signInWithEmailAndPassword(email, password))
+            .catch(err => {
+                console.error('Error signing in to Firebase Auth: ', err);
+                return Observable.throw(err)
+            }).do(result => {
+                // Authentication succcessful: Set this.currentUser, this.userToken, and this.authenticated.
+                const firebaseUser = result.toJSON();
+                this.currentUser = userData; // TODO: Can remove this? User Service works here!
+                this.userService.setActiveUser(userData);
+                this.userToken = firebaseUser.stsTokenManager.accessToken;
+                this.authenticated = true;
             });
     }
 
