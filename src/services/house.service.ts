9d3ce2f866ba2a1e9f108adf067611add954f431
John <jmseymour111@gmail.com>
2018-03-24 12:27:47 -0700

    added test route for roommates

--- src/services/house.service.ts
+++ src/services/house.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class HouseService {
+
+    _roommates: any[] = [];
+
+    get roommates() {
+        return this._roommates;
+    }
+
     constructor(private http: HttpClient) { }
 
     createHouse(houseName, houseCode) {
@@ -15,5 +23,18 @@ export class HouseService {
         console.log(houseName, houseCode);
         const house = { houseName, houseCode };
         return this.http.post('https://housepal-server.herokuapp.com/houses/join', house);
+    }
+
+    getRoommates() {
+        console.log('get roommates');
+        return this.http.get('https://housepal-server.herokuapp.com/users/roommates')
+            .catch(err => {
+                console.error('Error getting roommates: ', err);
+                return Observable.throw(err);
+            })
+            .do((res: any[]) => {
+                console.log('roommates (App): ', res);
+                this._roommates = res;
+            });
     }
 }
