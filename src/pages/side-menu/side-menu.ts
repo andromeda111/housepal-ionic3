20f0d777e9abacad1eed8ddf71f81f752f948b0d
andromeda111 <jmseymour111@gmail.com>
2018-03-25 18:35:30 -0700

    set up styling for remove roommate button

--- src/pages/side-menu/side-menu.ts
+++ src/pages/side-menu/side-menu.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../../services/auth.service';
 import { SigninPage } from '../user-setup/signin/signin';
 import { UserService } from '../../services/user.service';
 import { HouseService } from '../../services/house.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -15,6 +16,8 @@ export class SideMenuPage {
     activeMenu = 'Roommates';
     house: any = {};
     roommates: any = [];
+    removeForm: FormGroup;
+
 
     @Input() userName = '';
 
@@ -30,11 +33,41 @@ export class SideMenuPage {
         this.houseService.getRoommates()
             .do(() => this.roommates = this.houseService.roommates)
             .subscribe();
+
+            this.initializeForm()
     }
 
     selectMenu(menu: string) {
         this.activeMenu === menu ? this.activeMenu = '' : this.activeMenu = menu;
     }
+
+    private initializeForm() {
+        let name = null;
+    
+        this.removeForm = new FormGroup({
+          'name': new FormControl(name, Validators.required),
+        });
+      }
+
+    onRemoveFormSubmit() {
+        const value = this.removeForm.value;
+        console.log(value);
+        
+        // let ingredients = [];
+        // if (value.ingredients.length > 0) {
+        //   ingredients = value.ingredients.map(name => {
+        //     return {name: name, amount: 1}
+        //   })
+        // }
+        // if (this.mode == 'Edit') {
+        //   this.recipesService.updateRecipe(this.index, value.title, value.description, value.difficulty, ingredients)
+        // } else {
+        //   this.recipesService.addRecipe(value.title, value.description, value.difficulty, ingredients);
+        // }
+        // this.recipeForm.reset();
+        // this.navCtrl.popToRoot();
+      }
+
 
     signOut() {
         this.activeMenu = 'Sign Out';
