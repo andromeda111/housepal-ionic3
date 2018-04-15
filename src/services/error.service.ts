db4618de91d439ee5d1d19445d8969441b5787a3
andromeda111 <jmseymour111@gmail.com>
2018-04-15 15:14:52 -0700

    created error.service

---
+++ src/services/error.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { AlertController } from 'ionic-angular';
+
+@Injectable()
+export class ErrorService {
+
+    constructor(private alertCtrl: AlertController) { }
+
+
+}
