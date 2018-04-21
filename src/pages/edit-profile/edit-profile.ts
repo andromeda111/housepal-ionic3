6e77bcbfa8b73448b94b97fc7263517733010aa7
John <jmseymour111@gmail.com>
2018-04-20 19:16:32 -0700

    added camera code

--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -7,5 +8,26 @@ import { IonicPage } from 'ionic-angular';
     templateUrl: 'edit-profile.html'
 })
 export class EditProfilePage {
+
+    photo: any;
+
+    constructor(private camera: Camera) { }
+
+    editPhoto() {
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+        }
+
+        this.camera.getPicture(options).then((imageData) => {
+            // imageData is either a base64 encoded string or a file URI
+            // If it's base64:
+            this.photo = 'data:image/jpeg;base64,' + imageData;
+        }, (err) => {
+            // Handle error
+        });
+    }
 
 }
