5b7e4b0f3e5b3e6217da67cad05af11fe58114f7
John <jmseymour111@gmail.com>
2018-05-20 21:25:30 -0700

    minor temp fix

--- src/components/chore-card/chore-card.component.ts
+++ src/components/chore-card/chore-card.component.ts
@@ -31,6 +31,10 @@ export class ChoreCardComponent {
                 this.color = 'blue';
                 break;
         }
+
+        if (this.chore.done) {
+            this.done = true;
+        }
     }
 
     markDone(id) {
