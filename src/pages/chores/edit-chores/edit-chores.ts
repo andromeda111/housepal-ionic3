e2eb94bf8d69903868e06ee61c337a532da0a13b
andromeda111 <jmseymour111@gmail.com>
2018-05-29 21:15:50 -0700

    removed dupe page, touched up css

--- src/pages/chores/edit-chores/edit-chores.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
-
-@IonicPage()
-@Component({
-    selector: 'page-edit-chores',
-    templateUrl: 'edit-chores.html'
-})
-export class EditChoresPage { }
